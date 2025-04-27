a0a2846854924258654c72d57f5ed3595c0e6af5
itzmhizterlouis <itzmhizterlouis@gmail.com>
2025-04-27 14:02:59 +0100

    Bootstrap complete

---
+++ src/main/java/com/thelastcodebenders/bread_budget_backend/BreadBudgetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thelastcodebenders.bread_budget_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadBudgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadBudgetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thelastcodebenders/bread_budget_backend/BreadBudgetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thelastcodebenders.bread_budget_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadBudgetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
