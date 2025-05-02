242c1205b9f4189cd19a42fe6223cfe74caf16fb
itzmhizterlouis <itzmhizterlouis@gmail.com>
2025-05-02 21:50:44 +0100

    Ai optimization

--- src/main/java/com/thelastcodebenders/bread_budget_backend/adapters/GroqApiAdapter.java
+++ src/main/java/com/thelastcodebenders/bread_budget_backend/adapters/GroqApiAdapter.java
@@ -34,14 +34,23 @@ public class GroqApiAdapter {
 
         String preCommand = "This is a bank statement please can you generate insights on this bank statement" +
                 "Start your response with <response>" +
-                "Please i want two sections contained in tags give me a summary giving what i generally spend money inbetween the <summary>" +
-                " tag and advice on how i can reduce my spending's between <advice> tag. Make the summary between 70 and 90 words" +
-                " and make the advice between 70 and 90 words" +
+                "Please i want three sections contained in tags" +
+                " do not any form of '%' in your response please" +
+                " please do not give amounts spent instead give the amounts in terms of percentages" +
+                " you can reference amounts using words like 'a good amount' 'a small amount' and other relevant words" +
+                " but for the values in the data section please put them in form of percentages" +
+                " to get advice insights you can take inspiration from the book 'The richest man in Babylon'" +
+                " and 'The psychology of Money book' also" +
+                " give me a summary giving what i generally spend money inbetween the <summary>" +
+                " tag and advice on how i can reduce my spending's between <advice> tag. Make the summary between 120 and 150 words" +
+                " and make the advice between 120 and 150 words" +
                 " Please do not return anything irrelevant to the question." +
                 " I want you to give some data in this format also after the advice section inside a <data> tag" +
-                " in the form {`transactionCategory1`:`amount1`} without any backslashes(\\)" +
+                " in the form {`transactionCategory1`:`transactionAmountPercentage`} without any backslashes(\\)" +
                 " please follow my instructions for the data form strictly" +
-                "for debits prefix the transactionCategory with - for positive values prefix it with + sign" +
+                " please put only debits in the data section and make the categories as such" +
+                " transfers to personal people, online payments and electronic transfer levy" +
+//                "for debits prefix the transactionCategory with - for positive values prefix it with + sign" +
                 "Here are the contents of the file:\n";
 
         HttpHeaders headers = new HttpHeaders();
@@ -69,7 +78,7 @@ public class GroqApiAdapter {
         String advice = Objects.requireNonNull(CommonUtils.getStringBetween(response, "<advice>", "</advice>"));
 
         String data = Objects.requireNonNull(CommonUtils.getStringBetween(response, "<data>", "</data>"))
-                .replaceAll("[^a-zA-Z0-9{}\"`':, ]", "").replace("`", "\"")
+                .replaceAll("[^a-zA-Z0-9{}\"`':,% ]", "").replace("`", "\"")
                 .replace("'", "\"");
 
         log.info("Response is: {}", response);
